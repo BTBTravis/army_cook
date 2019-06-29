7ea8dae1edb17b14e21ca6652ea9f373cfa4d08d
Travis Shears <btbtravis@gmail.com>
2019-06-29 15:44:26 +0200

    Add sayings

--- armycook/cli.py
+++ armycook/cli.py
@@ -3,6 +3,7 @@ import glob
 import os
 import datetime
 import json
+import random
 
 @click.command()
 def cli():
@@ -12,10 +13,25 @@ def cli():
     og_date_parts = list(map(lambda x: int(x), og_date_parts)) # string --> int
     og_date = datetime.datetime(og_date_parts[0], og_date_parts[1], og_date_parts[2])
     # click.echo(json.dumps(og_date.year))
-    click.echo(json.dumps(getTweets()))
+    click.echo(json.dumps(get_tweets()))
 
-def getTweets():
-    # load master.txt
+def get_tweets():
+    master_txt = load_master_txt()
+    sayings = load_sayings()
+    get_random_saying = lambda: random.choice(sayings)
+    return list(map(lambda x: get_tweet(x, get_random_saying), master_txt))
+
+def get_tweet(txt, sayingFn):
+    tweet = sayingFn()
+    tweet = tweet.replace("$body", txt['body'])
+    tweet = tweet.replace("$title", txt['title'])
+    if(len(tweet) > 280):
+        return get_tweet(txt, sayingFn)
+    return tweet
+
+
+def load_master_txt():
+    """Load file containing text parts to be used in tweet"""
     txt_file  = open("/army/master.txt", "r")
     txt = txt_file.read()
     txt_parts = txt.split('--')
@@ -31,3 +47,11 @@ def getTweets():
 
     return txt_parts
 
+def load_sayings():
+    """Load txt file containing sayings part of the tweet"""
+    txt_file  = open("/army/sayings.txt", "r")
+    txt = txt_file.read()
+    txt_parts = txt.split("\n")
+    txt_parts = list(filter(lambda x: len(x) > 0, txt_parts))
+
+    return txt_parts
